6ad8664d6b65ef1039e3e1e1a02bfa020871b7a0
EC2 Default User <ec2-user@ip-172-31-20-235.us-west-2.compute.internal>
2018-04-24 01:06:26 +0000

    better face_detect

--- face_detect/detect.py
+++ face_detect/detect.py
@@ -3,12 +3,109 @@
 
 # import the necessary packages
 from imutils import face_utils
+from collections import OrderedDict
 import numpy as np
 import argparse
 import imutils
 import dlib
 import cv2
 import os
+
+# define a dictionary that maps the indexes of the facial
+# landmarks to specific face regions
+FACIAL_LANDMARKS_IDXS = OrderedDict([
+	("mouth", (48, 68)),
+	("right_eyebrow", (17, 22)),
+	("left_eyebrow", (22, 27)),
+	("right_eye", (36, 42)),
+	("left_eye", (42, 48)),
+	("nose", (27, 36)),
+	("jaw", (0, 17))
+])
+
+def rect_to_bb(rect):
+	# take a bounding predicted by dlib and convert it
+	# to the format (x, y, w, h) as we would normally do
+	# with OpenCV
+	x = rect.left()
+	y = rect.top()
+	w = rect.right() - x
+	h = rect.bottom() - y
+
+	# return a tuple of (x, y, w, h)
+	return (x, y, w, h)
+
+def shape_to_np(shape, dtype="int"):
+	# initialize the list of (x, y)-coordinates
+	coords = np.zeros((shape.num_parts, 2), dtype=dtype)
+
+	# loop over all facial landmarks and convert them
+	# to a 2-tuple of (x, y)-coordinates
+	for i in range(0, shape.num_parts):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+
+	# return the list of (x, y)-coordinates
+
+def visualize_facial_landmarks(image, shape, colors=None, alpha=0.75):
+	# create two copies of the input image -- one for the
+	# overlay and one for the final output image
+	overlay = image.copy()
+	output = image.copy()
+
+	# if the colors list is None, initialize it with a unique
+	# color for each facial landmark region
+	if colors is None:
+		colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
+			(168, 100, 168), (158, 163, 32),
+			(163, 38, 32), (180, 42, 220)]
+
+	# loop over the facial landmark regions individually
+	for (i, name) in enumerate(FACIAL_LANDMARKS_IDXS.keys()):
+		# grab the (x, y)-coordinates associated with the
+		# face landmark
+		(j, k) = FACIAL_LANDMARKS_IDXS[name]
+		pts = shape[j:k]
+
+		# check if are supposed to draw the jawline
+		if name == "jaw":
+			# since the jawline is a non-enclosed facial region,
+			# just draw lines between the (x, y)-coordinates
+                        left = True
+			for l in range(1, len(pts)):
+				ptA = tuple(pts[l - 1])
+				ptB = tuple(pts[l])
+                                print(ptA)
+                                print(ptB)
+                                ptSa = pts[l - 1]
+                                ptSb = pts[l]
+                                # if (pts[l - 1][1] >= pts[l][1] & left):
+                                if (l < len(pts)/2):
+                                    ptSa[0] += 4
+                                    ptSb[0] += 4
+                                    ptA = tuple(ptSa)
+                                    ptB = tuple(ptSb)
+                                # elif (pts[l - 1][1] <= pts[l][1] or not left):
+                                else:
+                                    left = False
+                                    ptSa[0] -= 4
+                                    ptSb[0] -= 4
+                                    ptA = tuple(ptSa)
+                                    ptB = tuple(ptSb)
+                                if l+1  == len(pts)/4 or l-2 == (len(pts)/2 + len(pts)/4):
+				    cv2.line(overlay, ptA, ptB, (68, 0, 204), 2)
+                                else:
+				    cv2.line(overlay, ptA, ptB, colors[i], 2)
+		# otherwise, compute the convex hull of the facial
+		# landmark coordinates points and display it
+		else:
+			hull = cv2.convexHull(pts)
+			cv2.drawContours(overlay, [hull], -1, colors[i], 2)
+
+	# apply the transparent overlay
+	cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+
+	# return the output image
+	return output
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -25,7 +122,7 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 
 # load the input image, resize it, and convert it to grayscale
 image = cv2.imread(args["image"])
-image = imutils.resize(image, width=500)
+image = imutils.resize(image, width=400)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # detect faces in the grayscale image
@@ -37,36 +134,18 @@ for (i, rect) in enumerate(rects):
 	# convert the landmark (x, y)-coordinates to a NumPy array
 	shape = predictor(gray, rect)
 	shape = face_utils.shape_to_np(shape)
-	#
-	# # loop over the face parts individually
-	# for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
-	# 	# clone the original image so we can draw on it, then
-	# 	# display the name of the face part on the image
-	# 	clone = image.copy()
-	# 	cv2.putText(clone, name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-	# 		0.7, (0, 0, 255), 2)
-	#
-	# 	# loop over the subset of facial landmarks, drawing the
-	# 	# specific face part
-	# 	for (x, y) in shape[i:j]:
-	# 		cv2.circle(clone, (x, y), 1, (0, 0, 255), -1)
-	#
-	# 	# extract the ROI of the face region as a separate image
-	# 	(x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
-	# 	roi = image[y:y + h, x:x + w]
-	# 	roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
-	#
-	# 	# show the particular face part
-	# 	cv2.imshow("ROI", roi)
-	# 	cv2.imshow("Image", clone)
-	# 	cv2.waitKey(0)
 
 	# visualize all facial landmarks with a transparent overlay
-	colors = [(170, 47, 35), (91, 61, 46), (91, 61, 46),
-			(206, 201, 198), (206, 201, 198),
-			(204, 157, 128), (204, 157, 128)]
-	output = face_utils.visualize_facial_landmarks(image, shape)
+	face_colors = [(12, 56, 204), (51, 31, 84), (51, 31, 84),
+			(145, 145, 145), (145, 145, 145),
+			(114,160,204), (114,160,204)]
+
+	output = visualize_facial_landmarks(image,shape,colors=face_colors, alpha=1)
 #	cv2.imshow("Image", output)
-	filename = os.path.splitext(os.path.basename(args["image"]))[0] + "_detect.jpg"
+        height, width = output.shape[:2]
+        title = os.path.splitext(os.path.basename(args["image"]))[0]
+	filename = "../colorization/samples/" + title + "_marked.bmp"
+        filename_original = "../colorization/samples/" + title + ".bmp"
 	cv2.imwrite(filename, output)
+        cv2.imwrite(filename_original, image)
 	cv2.waitKey(0)
